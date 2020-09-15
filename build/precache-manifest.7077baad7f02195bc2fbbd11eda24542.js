4efad00aca9d8ee7b771374abbd5d8d809db4e46
Tristan Emmerson <tristan@stickerslug.com>
2020-09-14 21:07:33 -0700

    refresh site

---
+++ build/precache-manifest.7077baad7f02195bc2fbbd11eda24542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1277ef49fbc58f85218190a3b11cd520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45805bd491a9dcf375ad",
+    "url": "/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "6ad7b04dcc8f04b80610",
+    "url": "/static/js/2.6b2ff276.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b2ff276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45805bd491a9dcf375ad",
+    "url": "/static/js/main.aa50ab10.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
